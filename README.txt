216d09c9b114b9d32f4c247050d08284dc9e3c03
Andrew <ablashch@gmail.com>
2019-11-22 13:21:41 +0200

    product version of ITClaster project

---
+++ README.txt
@@ -0,0 +1,4 @@
+Here is a final production of ITClaster Academy project.
+Theme:Website about Leather goods workshop.
+Customer: "Shyyan Workshop"-Leather good shop.Taras Shyyan-https://www.facebook.com/shyyanworkshop/
+Developers: Andrii Blashchuk and Stas Pechonkin
---
+++ js/main.js
@@ -0,0 +1,27 @@
+
+const anchors = document.querySelectorAll('header .nav-item > a')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
+
+$('section .gallery_block .wrmain .item a').fancybox({
+  thumbs: {
+    autoStart: true, // Display thumbnails on opening
+    hideOnClose: true, // Hide thumbnail grid when closing animation starts
+    parentEl: ".fancybox-container", // Container is injected into this element
+    axis: "y" // Vertical (y) or horizontal (x) scrolling
+  }
+
+});
+
+
